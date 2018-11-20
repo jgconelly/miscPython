412b85a06b353829094ed39dea6bf3070c89499a
azreal45 <jgconelly@gmail.com>
2018-11-19 20:55:27 -0800

    Parallelized with arg parser

--- pythongrep.py
+++ pythongrep.py
@@ -4,9 +4,17 @@ import multiprocessing as mp
 
 def main(): 
 	argparsegrp()
-	#process_input()
-	grep_term = "split"
-	grep_file_path = "/Users/Azreal/Documents/jcrosalind/dictionaries5.py"
+
+def argparsegrp():
+	parser = argparse.ArgumentParser()
+	#parser.add_argument("-c", help="A count of selected lines is written to a standard ouput")
+	parser.add_argument("grep", help="Nothing but grep")
+	parser.add_argument("grep_file_path" , help='File path to grep')
+	args = parser.parse_args()
+	full_grep = grep_process(args.grep, args.grep_file_path)
+	print (full_grep)
+
+def grep_process(grep_term, grep_file_path):
 	def process(line):
 		if grep_term in line:
 			print (line)
@@ -15,29 +23,8 @@ def main():
 			process(line)
 	result = re.match(grep_term, grep_file_path)
 
-def argparsegrp():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-c", help="A count of selected lines is written to a standard ouput")
-	args = parser.parse_args()
-	if args.c:
-		print (args.echo)
-
-#def process_input():
-#	grep_term = input("Please Enter Search Term:")
-#	grep_file_path = input("Please Enter File Path:")
-#	return grep_term, grep_file_path
-
-#def grep_process(grep_term, grep_file_path):
-#	if grep_term in line:
-#		print(line)
-#	with open(grep_file_path) as f:
-#		for line in f:
-#			process(line)
-#	result = re.match(grep_term, grep_file_path)
-
 
 if __name__ == '__main__':
 	p = mp.Process(target=main)
 	p.start()
 	p.join()
-    #main()
