f7deea4869f398d4169e78e6d6c4d0d7f15da6dc
azreal45 <jgconelly@gmail.com>
2018-12-15 16:33:51 -0800

    More modifications

--- baseball.py
+++ baseball.py
@@ -6,32 +6,11 @@ import csv
 
 def main():
     database = "/Users/Azreal/Documents/miscPython/test.db"
- 
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
-                                    ); """
- 
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                                );"""
- 
     # create a database connection
     conn = create_connection(database)
     if conn is not None:
-        # create projects table
-        create_table(conn, sql_create_projects_table)
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
+    	imported_df = pd.read_csv("/Users/Azreal/Downloads/baseballdatabank-master/core/Parks.csv")
+    	imported_df.to_sql("parks", con=conn, index=False, if_exists='replace')
     else:
         print("Error! cannot create the database connection.")
 
