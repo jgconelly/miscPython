225fdecfc7edbcd6d8832ab760d00f963182be88
azreal45 <jgconelly@gmail.com>
2018-12-29 12:09:48 -0500

    Adding in functions for batting average/obp

---
+++ baseballsim.py
@@ -0,0 +1,34 @@
+"""Sabermetrics Part 2 --- Baseball game simu
+
+References
+----------
+
+* http://www.seanlahman.com/files/database/readme2017.txt
+using mookie betts 2017 season to start
+
+"""
+
+import pandas as pd
+
+
+
+def main():
+
+def getTeams():
+
+def getBattingAverage(hits, atbats):
+	#atbats = 628
+	#hits = 166
+	return (round(hits / atbats, 3)
+
+def getOnbasePercentage(hits, walks, hitsbypitch, plateapp):
+	#atbats = 628
+	#hits = 166
+	#walks = 77
+	#hbp = 2
+	return (round(((hits + walks + hbp) / atbats), 3))
+
+
+if __name__ = '__main__':
+	main()
+
