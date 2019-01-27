fb56829b07975578543dfb0b472ea7a7a8432c82
James Conelly <jgconelly@gmail.com>
2019-01-26 16:14:50 -0800

    Adding loop for at bats

--- baseballsim.py
+++ baseballsim.py
@@ -19,42 +19,44 @@ def getbattingaverage(hits, atbats):
 def getonbasepercentage(hits, walks, hbp, atbats):
 	return (round(((hits + walks + hbp) / atbats), 3))
 
-#def getpitchingstats(per_strikes, per_balls, ipouts):
-#	percentage_strikes = 
+def choose_length():
+	number_at_bats = int(input("Enter length for simulator: "))
+	return number_at_bats
 
+def ballinplay(number_at_bats):
+	loop = 0
 
-def ballinplay():
-	batting = getBattingAverage(166, 628)
-	rnum = round(random.random(), 3)
-	balls = 0
-	strikes = 0
-	outs = 0
+	while loop <= number_at_bats:
+		rnum = round(random.random(), 3)
+		batting = getbattingaverage(166, 628)
+		balls = 0
+		strikes = 0
+		outs = 0
 
-	while strikes < 3:
-		print (rnum)
-		if batting >= rnum:
-			print ("There is a ball in play!")
-			strikes = 4
+		while strikes < 3:
+			print (rnum)
+			if batting >= rnum:
+				print ("There is a ball in play!")
+				strikes = 4
+			else:
+				print ("There is no ball in play.")
+				rnum = round(random.random(), 3)
+				strikes += 1
+
+		if strikes == 3:
+			print("The player has struck out.")
+			outs += 1
+			loop +=1
 		else:
-			print ("There is no ball in play.")
-			rnum = round(random.random(), 3)
-			strikes += 1
+			print("The player is on base.")
+			loop +=1
 
-	if strikes == 3:
-		print("The player has struck out.")
-		outs += 1
-	else:
-		print("The player is on base.")
-
-	if outs == 3:
-		print("The inning has ended.")
+		if outs == 3:
+			print("The inning has ended.")
 
 
 def main():
-	ba = getBattingAverage(166, 628)
-	obp = getOnbasePercentage(166, 77, 2, 628)
-	print (ba, obp)
-	ballInPlay()
+	ballinplay(choose_length())
 
 if __name__ == '__main__':
 	main()
